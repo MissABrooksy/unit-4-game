b78bbada8b5c04b779ecd0e72ebd40b91599d49e
Andrea <andreakwilliams1@gmail.com>
2019-09-02 12:13:58 -0400

    setting up variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//setup variables
+var  numbertoguess = floor.Math(math.Random()* 121);
+
+var crystal1 = 2 < floor.math(math.Random()*10) < 13;
+var crystal2 = 2 < floor.Math(math.Randon()*10) < 13;
+var crystal3 = 2 < floor.Math(math.Random()*10) < 13;
+var crystal4 = 2 < floor.Math(math.Random()*10) < 13;
+
+var score;
+var wins = 0;
+var losses = 0;
+
+//click buttons
+
+
