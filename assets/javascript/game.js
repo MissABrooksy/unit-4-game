5b4929a6607e2941ae2aa704a70139bb5ae9bf45
Andrea <andreakwilliams1@gmail.com>
2019-09-04 00:05:06 -0400

    more edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var  numbertoguess = Math.floor(Math.random()* 121);
 
 
  var crystal1 = Math.ceil(Math.random()*12) ;
-var crystal2 =  Math.ceil(Math.random()*12) ;
+ var crystal2 = Math.ceil(Math.random()*12) ;
  var crystal3 = Math.ceil(Math.random()*12) ;
  var crystal4 = Math.ceil(Math.random()*12) ;
 
@@ -11,6 +11,7 @@ var crystal2 =  Math.ceil(Math.random()*12) ;
  var wins = 0;
  var losses = 0;
  
+ $("#number-to-guess").html(numbertoguess);
 
 $("#diamond").on("click", addCrystal1ToScore);
 
@@ -19,27 +20,33 @@ function addCrystal1ToScore() {
   $("#score-box").html(score);
 }
 
-addCrystal1ToScore(5, 7)
-// $("#crystal2").on("click", crystal22());
+addCrystal1ToScore();
 
-// $("#crystal3").on("click", crystal33());
+$("#bluediamond").on("click", addCrystal2ToScore);
 
-// $("#crystal4").on("click", crystal44());
+function addCrystal2ToScore() {
+  score = score + crystal2; 
+  $("#score-box").html(score);
+}
+  addCrystal2ToScore();
+
+ $("#pinkruby").on("click", addCrystal3ToScore);
+
+ function addCrystal3ToScore() {
+  score = score + crystal3; 
+  $("#score-box").html(score);
+ }
+  addCrystal3ToScore();
+ 
+ $("#greendiamond").on("click", addCrystal4ToScore);
+ function addCrystal4ToScore() {
+  score = score + crystal4; 
+  $("#score-box").html(score);
+  } addCrystal4ToScore();
+
+ 
+ 
+ $("#number-to-guess").html(numbertoguess);
 
 
  
-// document.getElementById('diamond').onclick = function crystal1() {
-
-//   document.getElementById('diamond').innerHTML = sum;
-//   document.getElementById('diamond').style = "Color: red";
-//     }
-  
-// function crystal11(crystal1, score) {
-//   var sum = crystal1 + score;
-//   return sum;
-
-// }
-
-
-
-
